e315070b94a63f22338353ac55416ecc2002416d
IshikawaRasoto <ishikawarasoto@gmail.com>
2023-10-13 02:13:06 -0300

    Atualizações do Sistema

--- 2023-2/Telemetria/ImperadorTelemetria.py
+++ 2023-2/Telemetria/ImperadorTelemetria.py
@@ -13,7 +13,6 @@ from tkinter import ttk
 import os
 from PIL import Image, ImageTk
 import serial.tools.list_ports # pyserial
-import threading
 import continuous_threading
 import datetime
 import numpy
@@ -50,20 +49,36 @@ baudrate = 9600
 velocidadeAtual = 0
 rpmAtual = 0
 freioAtual  = False
-temperaturaAtual = True
-bateriaAtual = True
+temperaturaAtual = 0
+bateriaAtual = 0
+aceleracaoX = 0
+aceleracaoY = 0
+aceleracaoZ = 0
+anguloX = 0
+anguloY = 0
+anguloZ = 0
 
 
 dataVel = []
 dataRPM = []
+dataBateria = []
+dataTemperatura = []
+dataFreio = []
 dataTempo = []
-valoresX = []
-fGrafico = Figure(figsize=(7,7), dpi=100)
-grafVel = fGrafico.add_subplot(211)
-grafRPM = fGrafico.add_subplot(212)
 
-for i in range(0, 60):
-    valoresX.append(i)
+dataAccX = []
+dataAccY = []
+dataAccZ = []
+dataAngX = []
+dataAngY = []
+dataAngZ = []
+
+
+fGrafico = Figure(figsize=(14, 7), dpi=100)
+grafVel = fGrafico.add_subplot(221)
+grafRPM = fGrafico.add_subplot(222)
+grafTemp = fGrafico.add_subplot(223)
+grafBat = fGrafico.add_subplot(224)
 
 
 ###### Eventos
@@ -87,16 +102,15 @@ def eventoSelectCOM(COM_recebido):
 
     COM_recebido = varPortaCOM.get()
 
-    COM_recebido = COM_recebido[2:32]
-    
-    print("Porta COM recebida: " + COM_recebido)
+    COM_recebido = COM_recebido[0:4]
+
+    print(COM_recebido)
+
     
     for i in range(0, len(portList)):
-        print(portList[i])
-        if portList[i] == COM_recebido:
-            print("COM recebida" + portList[i][0:4])
+
+        if COM_recebido in  portList[i]:
             portaCOM = portList[i][0:4]
-            print("Porta COM Selecionada: " + portaCOM)
 
     return
 
@@ -136,7 +150,16 @@ def eventoExportarExcel():
     data = {
         'horario': dataTempo,
         'velocidade': dataVel,
-        'RPM': dataRPM
+        'RPM': dataRPM,
+        'Temperatura': dataTemperatura,
+        'Bateria': dataBateria,
+        'Freio': dataFreio,
+        'AccX': aceleracaoX,
+        'AccY': aceleracaoY,
+        'AccZ': aceleracaoZ,
+        'AngX': anguloX,
+        'AngY': anguloY,
+        'AngZ': anguloZ
     }
 
     df = pd.DataFrame(data)
@@ -149,7 +172,7 @@ def eventoExportarExcel():
 ###### JANELA PRINCIPAL
 
 janela = tkinter.Tk()
-janela.geometry("1280x720")
+janela.geometry("1920x950")
 janela.title("Imperador - Telemetria")
 janela.grid_rowconfigure(0, weight=1)
 janela.grid_columnconfigure(1, weight=1)   
@@ -196,7 +219,7 @@ frameConexao.grid_columnconfigure(0, weight=1)
 frameConexao.grid(row=0, column=1, sticky="nsew")
 
 frameTelemetria = tkinter.Frame(janela)
-frameTelemetria.grid_columnconfigure(2, weight=1)
+frameTelemetria.grid_rowconfigure(1, weight=1)
 
 frameLog = tkinter.Frame(janela)
 frameLog.grid_columnconfigure(0, weight=1)
@@ -231,7 +254,7 @@ portaCOMLabel.grid(row=3, column=0, padx=20, pady=0)
 varPortaCOM = tkinter.StringVar()
 varPortaCOM.set("COM3")
 
-portaCOMSelector = tkinter.OptionMenu(frameConexao, varPortaCOM, value=portList, command=eventoSelectCOM)
+portaCOMSelector = tkinter.OptionMenu(frameConexao, varPortaCOM, *portList, command=eventoSelectCOM)
 portaCOMSelector.grid(row=4, column=0, padx=20, pady=10)
 
 conectarBotao = tkinter.Button(frameConexao, text="Conectar" if statusConexao == False else "Desconectar", image = imagemConectar, command=eventoBotaoConectar)
@@ -254,15 +277,34 @@ enviarBotao.grid(row=9, column=0, padx=20, pady=0)
 
 ###### FRAME DE TELEMETRIA
 
-frameTelemetriaEsquerdo = tkinter.Frame(frameTelemetria)
+frameTelemetriaSuperior = tkinter.Frame(frameTelemetria)
+frameTelemetriaSuperior.grid_columnconfigure(1, weight=2)
+frameTelemetriaSuperior.grid(row=0, column=0, sticky="nsew")
+
+frameTelemetriaInferior = tkinter.Frame(frameTelemetria)
+frameTelemetriaInferior.grid_columnconfigure(3, weight=1)
+frameTelemetriaInferior.grid_rowconfigure(2, weight=1)
+frameTelemetriaInferior.grid(row=1, column=0, sticky="nsew")
+
+frameTelemetriaEsquerdo = tkinter.Frame(frameTelemetriaSuperior)
 frameTelemetriaEsquerdo.grid_columnconfigure(0, weight=1)
 frameTelemetriaEsquerdo.grid(row=0, column=0, sticky="nsew")
 
-frameTelemetriaDireito = tkinter.Frame(frameTelemetria)
+frameTelemetriaDireito = tkinter.Frame(frameTelemetriaSuperior)
 frameTelemetriaDireito.grid_columnconfigure(0, weight=1)
 frameTelemetriaDireito.grid(row=0, column=1, sticky="nsew")
 
-velocidadeLabel = tkinter.Label(frameTelemetriaEsquerdo, text = "Velocidade: 0 km/h", font=fonteTitulo, image=imagemVelocidade, compound="top")
+frameAceleracao = tkinter.Frame(frameTelemetriaInferior)
+frameAceleracao.grid_rowconfigure(2, weight=1)
+frameAceleracao.grid(row=0, column=0, sticky="nsew")
+
+frameAngulo = tkinter.Frame(frameTelemetriaInferior)
+frameAngulo.grid_rowconfigure(2, weight=1)
+frameAngulo.grid(row=1, column=0, sticky="nsew")
+
+###########
+
+velocidadeLabel = tkinter.Label(frameTelemetriaEsquerdo, text="Velocidade: 0 km/h", font=fonteTitulo, image=imagemVelocidade, compound="top")
 velocidadeLabel.grid(row=0, column=0, padx=20, pady=20)
 
 rpmLabel = tkinter.Label(frameTelemetriaEsquerdo, text="RPM: 0", font=fonteTitulo, image=imagemRpm, compound="top")
@@ -271,13 +313,13 @@ rpmLabel.grid(row=1, column=0, padx=20, pady=5)
 rpmProgressBar = ttk.Progressbar(frameTelemetriaEsquerdo, orient="horizontal", length=200)
 rpmProgressBar.grid(column=0, row=2, padx=10, pady=0)
 
-temperaturaLabel = tkinter.Label(frameTelemetriaEsquerdo, text="Temperatura", font=fonteTitulo, image=imagemTemperatura, compound="top")
+temperaturaLabel = tkinter.Label(frameTelemetriaEsquerdo, text="Temperatura: 0 ºC", font=fonteTitulo, image=imagemTemperatura, compound="top")
 temperaturaLabel.grid(row=3, column=0, padx=20, pady=20)
 
 temperaturaAviso = tkinter.Label(frameTelemetriaEsquerdo, text="OK", font=fonteTitulo, fg="green")
 temperaturaAviso.grid(row=4, column=0, padx=20, pady=0)
 
-bateriaLabel = tkinter.Label(frameTelemetriaEsquerdo, text="Bateria", font=fonteTitulo, image=imagemBateria, compound="top")
+bateriaLabel = tkinter.Label(frameTelemetriaEsquerdo, text="Bateria: 0.0 V", font=fonteTitulo, image=imagemBateria, compound="top")
 bateriaLabel.grid(row=5, column=0, padx=20, pady=20)
 
 bateriaAviso = tkinter.Label(frameTelemetriaEsquerdo, text="OK", font=fonteTitulo, fg="green")
@@ -285,6 +327,26 @@ bateriaAviso.grid(row=6, column=0, padx=20, pady=0)
 
 freioLabel = tkinter.Label(frameTelemetriaEsquerdo, text=("   Freio: Desativado"), font=fonteTitulo, image=imagemFreio, compound="left")
 freioLabel.grid(row=7, column=0, padx=20, pady=50)
+
+################
+
+aceleracaoxLabel = tkinter.Label(frameTelemetriaInferior, text=("Aceleração X: " + str(aceleracaoX) + " G"), font=fonteTitulo, compound="left")
+aceleracaoxLabel.grid(row=0, column=0, padx=20, pady=20)
+
+aceleracaoYLabel = tkinter.Label(frameTelemetriaInferior, text=("Aceleração Y: " + str(aceleracaoY) + " G"), font=fonteTitulo, compound="left")
+aceleracaoYLabel.grid(row=0, column=1, padx=20, pady=20)
+
+aceleracaoZLabel = tkinter.Label(frameTelemetriaInferior, text=("Aceleração Z: " + str(aceleracaoZ) + " G"), font=fonteTitulo, compound="left")
+aceleracaoZLabel.grid(row=0, column=2, padx=20, pady=20)
+
+anguloXLabel = tkinter.Label(frameTelemetriaInferior, text=("Ângulo X: " + str(anguloX) + "º"), font=fonteTitulo, compound="left")
+anguloXLabel.grid(row=1, column=0, padx=20, pady=20)
+
+anguloYLabel = tkinter.Label(frameTelemetriaInferior, text=("Ângulo Y: " + str(anguloY) + "º"), font=fonteTitulo, compound="left")
+anguloYLabel.grid(row=1, column=1, padx=20, pady=20)
+
+anguloZLabel = tkinter.Label(frameTelemetriaInferior, text=("Ângulo Z: " + str(anguloZ) + "º"), font=fonteTitulo, compound="left")
+anguloZLabel.grid(row=1, column=2, padx=20, pady=20)
 
 
 
@@ -316,7 +378,7 @@ def selecionarFrame(nome):
         for onePort in ports:
             portList.append(str(onePort))
 
-        portaCOMSelector = tkinter.OptionMenu(frameConexao, varPortaCOM, value=portList, command=eventoSelectCOM)
+        portaCOMSelector = tkinter.OptionMenu(frameConexao, varPortaCOM, *portList, command=eventoSelectCOM)
         portaCOMSelector.grid(row=4, column=0, padx=20, pady=10)
 
     else:
@@ -363,6 +425,14 @@ def lerSerial():
     global freioAtual
     global bateriaAtual
 
+    global aceleracaoX
+    global aceleracaoY
+    global aceleracaoZ
+
+    global anguloX
+    global anguloY
+    global anguloZ
+
     if statusConexao == False:
         return
     
@@ -379,32 +449,44 @@ def lerSerial():
     texto = serialTexto
     textoRecebido.insert("0.0", horario + " -> " + texto)
 
-    ####
+    #### FORMATO ATUAL DA STRING: V**R***T***B**.*F*x*.*y*.*z*.*X***.*Y***.*Z***.*
 
-    #velocidadeAtual = int(texto[texto.index("V") + 1: texto.index("T")])
-    #dataVel.append(velocidadeAtual)
+    velocidadeAtual = int(texto[texto.index("V") + 1: texto.index("R")])
+    dataVel.append(velocidadeAtual)
 
-    #rpmAtual = int(texto[texto.index("R") + 1: texto.index("V")])*10
-    #dataRPM.append(rpmAtual)
+    rpmAtual = int(texto[texto.index("R") + 1: texto.index("T")])*10
+    dataRPM.append(rpmAtual)
 
-    #temperatura_string = texto[texto.index("T") + 1: texto.index("F")]
-    #freio_string = texto[texto.index("F") + 1: texto.index("B")]
-    #bateria_string = texto[texto.index("B") + 1: len(texto) - 1]
+    temperaturaAtual = int(texto[texto.index("T") + 1: texto.index("B")])
+    dataTemperatura.append(temperaturaAtual)
 
-    #if temperatura_string == "0":
-    #    temperaturaAtual = False
-    #else:
-    #    temperaturaAtual = True
+    bateriaAtual = float(texto[texto.index("B") + 1: texto.index("F")])
+    dataBateria.append(bateriaAtual)
 
-    #if freio_string == "0":
-    #    freioAtual = False
-    #else:
-    #    freioAtual = True
-    
-    #if bateria_string == "0":
-    #    bateriaAtual = False
-    #else:
-    #    bateriaAtual = True
+
+    freio_string = texto[texto.index("F") + 1: texto.index("x")]
+
+    if freio_string == "0":
+       freioAtual = False
+    else:
+        freioAtual = True
+
+    dataFreio.append(freioAtual)
+
+
+    aceleracaoX = float(texto[texto.index("x") + 1 : texto.index("y")])
+    dataAccX.append(aceleracaoX)
+    aceleracaoY = float(texto[texto.index("y") + 1 : texto.index("z")])
+    dataAccY.append(aceleracaoY)
+    aceleracaoZ = float(texto[texto.index("z") + 1 : texto.index("X")])
+    dataAccZ.append(aceleracaoZ)
+
+    anguloX = float(texto[texto.index("X") + 1 : texto.index("Y")])
+    dataAngX.append(anguloX)
+    anguloY = float(texto[texto.index("Y") + 1 : texto.index("Z")])
+    dataAngY.append(anguloY)
+    anguloZ = float(texto[texto.index("Z") + 1 : len(texto)-1])
+    dataAngZ.append(anguloZ)
 
     return
 
@@ -424,12 +506,15 @@ def atualizar():
     rpmLabel.configure(text=("RPM" + str(rpmAtual)))
     rpmProgressBar['value'] = (rpmAtual/4000)*100
 
-    if temperaturaAtual == True:
+    temperaturaLabel.configure(text = ("Temperatura: " + str(temperaturaAtual) + " ºC"))
+    bateriaLabel.configure(text = ("Bateria: " + str(bateriaAtual) + " V"))
+
+    if temperaturaAtual > 75:
         temperaturaAviso.configure(fg="red", text="ALERTA - ALTA")
     else:
         temperaturaAviso.configure(fg="green", text="OK")
 
-    if bateriaAtual == True:
+    if bateriaAtual < 12:
         bateriaAviso.configure(fg="red", text="Alerta")
     else:
         bateriaAviso.configure(fg="green", text="OK")
@@ -442,29 +527,44 @@ def atualizar():
     return
 
 
-def animacaoGraficos():
+def animacaoGraficos(self):
 
     dataVelGrafico = dataVel[-60:]
     dataRPMGrafico = dataRPM[-60:]
+    dataTempGrafico = dataTemperatura[-60:]
+    dataBatGrafico = dataBateria[-60:]
+    dataTempoGrafico = dataTempo[-60:]
 
     grafVel.clear()
     grafRPM.clear()
+    grafTemp.clear()
+    grafBat.clear()
 
-    grafVel.plot(valoresX, dataVelGrafico, 'b')
-    grafRPM.plot(valoresX, dataRPMGrafico, 'b')
+    grafVel.plot(dataTempoGrafico, dataVelGrafico, 'b')
+    grafRPM.plot(dataTempoGrafico, dataRPMGrafico, 'b')
+    grafTemp.plot(dataTempoGrafico, dataTempGrafico, 'b')
+    grafBat.plot(dataTempoGrafico, dataBatGrafico, 'b')
 
     grafVel.set_title("Velocidade")
     grafRPM.set_title("RPM")
+    grafTemp.set_title("Temperatura")
+    grafBat.set_title("Bateria")
 
     grafVel.set_ylabel("km/h")
-    grafVel.set_xlabel("s")
     grafVel.set_xlim(0, 60)
     grafVel.set_ylim(0, 55)
 
     grafRPM.set_ylabel("RPM")
-    grafRPM.set_xlabel("s")
     grafRPM.set_xlim(0, 60)
     grafRPM.set_ylim(0, 4500)
+
+    grafTemp.set_ylabel("ºC")
+    grafTemp.set_xlim(0, 60)
+    grafTemp.set_ylim(0, 135)
+
+    grafBat.set_ylabel("V")
+    grafBat.set_xlim(0, 60)
+    grafBat.set_yticks(range(0, 14))
 
     return
 
@@ -477,30 +577,26 @@ frameNavegacaoLogo.grid(row=0, column=0, padx=20, pady=0)
 frameNavegacaoTitulo = tkinter.Label(frameNavegacao, text="Telemetria Imperador", compound="left", font=fonteTitulo)
 frameNavegacaoTitulo.grid(row=1, column=0, padx=20, pady=0)
 
-NavegacaoBotaoConexao = tkinter.Button(frameNavegacao, text=" Conexão", font = fonteTitulo, compound=tkinter.LEFT, image=imagemConexao, command=eventoBotaoConexao)
+NavegacaoBotaoConexao = tkinter.Button(frameNavegacao, text=" Conexão", font=fonteTitulo, compound=tkinter.LEFT, image=imagemConexao, command=eventoBotaoConexao)
 NavegacaoBotaoConexao.grid(row=2, column=0, sticky="nsew", padx=10, pady=5)
 
-NavegacaoBotaoTelemetria = tkinter.Button(frameNavegacao, text=" Telemetria", font = fonteTitulo, compound=tkinter.LEFT, image=imagemTelemetria, command=eventoBotaoTelemetria)
+NavegacaoBotaoTelemetria = tkinter.Button(frameNavegacao, text=" Telemetria", font=fonteTitulo, compound=tkinter.LEFT, image=imagemTelemetria, command=eventoBotaoTelemetria)
 NavegacaoBotaoTelemetria.grid(row=3, column=0, sticky="nsew", padx=10, pady=5)
 
 NavegacaoBotaoLog = tkinter.Button(frameNavegacao, text=" Log", font = fonteTitulo, compound=tkinter.LEFT, image=imagemLog, command=eventoBotaoLog)
 NavegacaoBotaoLog.grid(row=4, column=0, sticky="nsew", padx=10, pady=5)
 
 
-###### Gráficos
 
+
+###### Gráficos
 
 graficos = FigureCanvasTkAgg(fGrafico, master=frameTelemetriaDireito)
 graficos.draw()
 graficos.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand = False)
-graficos._tkcanvas.pack()
 graficos._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand = False)
 
-ani = FuncAnimation(fGrafico, animacaoGraficos, interval=250, blit=False)
-
-
-
-
+ani = FuncAnimation(fGrafico, animacaoGraficos, interval=1000, blit=False)
 
 
 
@@ -510,8 +606,6 @@ threadSerial = continuous_threading.PeriodicThread(0.5, lerSerial)
 threadAtualizar = continuous_threading.PeriodicThread(0.5, atualizar)
 threadSerial.start()
 threadAtualizar.start()
-
-
 
 janela.mainloop()
 
